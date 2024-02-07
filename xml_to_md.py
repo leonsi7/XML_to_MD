6bfa27659de6b6422360397440a9b76fcbd78ea8
leonsi7 <104212258+leonsi7@users.noreply.github.com>
2024-02-07 09:45:52 +0700

    Second version

--- xml_to_md.py
+++ xml_to_md.py
@@ -1,49 +1,58 @@
-import os
-import xml.etree.ElementTree as ET
+# Tool for Gama Platform
+# Code written by Léon Sillano - IRD/ACROSS
 
+import os
+import xml.etree.ElementTree as et
+
+# Colors for log messages
+class bcolors:
+    HEADER = '\033[95m'
+    OKGREEN = '\033[92m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+# Parsing XML file and creating MD
 def xml_to_md(xml_file, md_file):
-    tree = ET.parse(xml_file)
+
+    tree = et.parse(xml_file)
     root = tree.getroot()
 
     with open(md_file, 'w', encoding='utf-8') as md:
         
+        # ABOUT CONSTANTS #
         constants_root = root.find('constants')
         if constants_root is not None:
-            md.write("## Constants\n\n")
             constants = constants_root.findall('constant')
+            if len(constants) > 0 :
+                md.write("## Constants\n\n")
             for constant in constants:
                 md.write(f"### {constant.get('name').replace('#', '').title()}\n\n")
-                
                 categories = constant.find('categories').findall('category')
                 for category in categories:
                     md.write(f"**Category**: {category.get('id').replace('_', ' ').title()}\n")
-                
                 doc_result = constant.find('documentation').find('result')
                 doc_returns = constant.find('documentation').find('returns')
-                
                 if doc_result is not None:
                     md.write(f"\n**Result**: {doc_result.text}\n")
                 if doc_returns is not None:
                     md.write(f"\n**Returns**: {doc_returns.text}\n")
-                
                 md.write("\n---\n")
 
+        # ABOUT CATEGORIES #
         operators_categories = root.find('operatorsCategories')
         if operators_categories is not None:
-            md.write("## Operators\n\n")
+            if (len(operators_categories)) > 0:
+                md.write("## Operators\n\n")
             for category in operators_categories.iter('category'):
-                md.write(f"### {category.get('id').replace('_', ' ').title()}\n\n")
-                
                 operators = root.find('operators')
                 if operators is not None:
+                    md.write(f"### {category.get('id').replace('_', ' ').title()}\n\n")
                     operators_list = operators.findall('operator')
                     for operator in operators_list:
                         continue_parsing = False
                         if operator.find("operatorCategories") is None:
                             continue
                         for opcategories in operator.find("operatorCategories").findall("category"):
-                            print(opcategories.get('id'))
-                            print(" ", category.get("id"))
                             if opcategories.get('id') == category.get("id") :
                                 continue_parsing = True
                         if continue_parsing == False:
@@ -71,53 +80,47 @@ def xml_to_md(xml_file, md_file):
                             examples = operator.find('documentation/usagesExamples/usage/examples').findall('example')
                             for example in examples:
                                 md.write(f"\n - Code: ```\n{example.get('code')} ```\n \n - Returns:  ```\n{example.get('equals')} ```\n\n")
-                        
                         md.write("\n---\n")
 
-
+        # ABOUT SPECIES #
         species = root.find('speciess').findall('species')
-        
+        if len(species) > 0:
+            md.write("## Species\n\n")
         for specie in species:
-            md.write(f"## {specie.get('id').replace('_', ' ').title()}\n\n")
-            
+            md.write(f"### {specie.get('id').replace('_', ' ').title()}\n\n")
             actions = specie.find('actions').findall('action')
-            
             for action in actions:
-                md.write(f"### {action.get('name')}\n\n")
-                
+                md.write(f"#### {action.get('name')}\n\n")
                 args = action.find('args').findall('arg')
                 for arg in args:
                     md.write(f"- *{arg.get('name')}* ({arg.get('type')})\n")
-                
                 doc_result = action.find('documentation').find('result')
                 doc_returns = action.find('documentation').find('returns')
-                
                 if doc_result is not None:
                     md.write(f"\n**Result**: {doc_result.text}\n")
                 if doc_returns is not None:
                     md.write(f"\n**Returns**: {doc_returns.text}\n")
-                
                 md.write("\n---\n")
 
-"""
-# Demander à l'utilisateur d'entrer le nom du fichier XML
-xml_input = input("Enter XML file or path: ")
-
-# Vérifier si le fichier existe dans le dossier local ou s'il s'agit d'un chemin absolu
+print(bcolors.HEADER + "Welcome to XML to MD program !" + bcolors.ENDC)
+# Get the file name
+xml_input = input("--> Enter XML file or path: ")
 if os.path.isfile(xml_input):
     xml_path = xml_input
 else:
     if os.path.isabs(xml_input):
         xml_path = xml_input
     else:
-        print("The file selected is not in the current folder or not an absolut file path.")
+        print(bcolors.FAIL + "!!> The file selected is not in the current folder or not an absolut file path."+bcolors.ENDC)
         exit()
 
-# Construire le nom du fichier de sortie avec l'extension .md
+# Creating the ouput file name
 md_output = os.path.splitext(xml_path)[0] + ".md"
-"""
-xml_path = "kDJaymRj.xml"
-md_output = "kDJaymRj.md"
-# Utilisation du script avec le chemin du fichier XML et le nom du fichier de sortie
-xml_to_md(xml_path, md_output)
-print(md_output + " has been successfully created.")
+
+# XML to MD
+try:
+    xml_to_md(xml_path, md_output)
+    print(bcolors.OKGREEN + "--> " + md_output + " has been successfully created." + bcolors.ENDC)
+except:
+    print(bcolors.FAIL + "!!> An error occurred when parsing XML file"+bcolors.ENDC)
+
